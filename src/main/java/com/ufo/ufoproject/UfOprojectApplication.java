31e5dafc77e68bd781997acf15cc972c43562c74
hyuk <tig05205@naver.com>
2023-11-10 15:12:08 +0900

    first11-10

---
+++ src/main/java/com/ufo/ufoproject/UfOprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ufo.ufoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfOprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UfOprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ufo/ufoproject/UfOprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufo.ufoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfOprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
