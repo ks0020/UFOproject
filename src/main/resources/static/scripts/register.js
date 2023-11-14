6420aa6cb992bff29be3b23e89eedfca8e0342fe
MinBumkim1901 <chngdch3@naver.com>
2023-11-14 12:44:39 +0900

    로그인 만드는중..... 11/14 12:43분

--- src/main/resources/static/scripts/register.js
+++ src/main/resources/static/scripts/register.js
@@ -33,5 +33,191 @@ registerForm['next'].addEventListener('click', e=> {
     step_2.style.display = 'block';
 });
 
+registerForm['check_email'].addEventListener('click', e => {
+
+    if (registerForm['email'].value === '') {
+        dialogCover.show();
+        duplicateLayer.show({
+            content: '이메일을 입력해주세요',
+            onConfirm: e => {
+                e.preventDefault();
+                dialogCover.hide();
+                duplicateLayer.hide();
+            }
+        });
+        return;
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(registerForm['email'].value)) {
+        dialogCover.show();
+        duplicateLayer.show({
+            content: '올바른 이메일 형식이 아닙니다',
+            onConfirm: e => {
+                e.preventDefault();
+                dialogCover.hide();
+                duplicateLayer.hide();
+            }
+        });
+        return;
+    }
+})
+//이메일 검사
+
+registerForm['check_nickname'].addEventListener('click', e => {
+    if (registerForm['nickname'].value === '') {
+        dialogCover.show();
+        duplicateLayer.show({
+            content: '닉네임을 입력해주세요',
+            onConfirm: e => {
+                e.preventDefault();
+                dialogCover.hide();
+                duplicateLayer.hide();
+            }
+        });
+        return;
+    }
+
+    const nicknameRegex = /^[a-zA-Z가-힣]{2,8}$/;
+    if(!nicknameRegex.test(registerForm['nickname'].value)){
+        dialogCover.show();
+        duplicateLayer.show({
+            content: '올바른 닉네임을 입력해주세요',
+            onConfirm: e => {
+                e.preventDefault();
+                dialogCover.hide();
+                duplicateLayer.hide();
+            }
+        });
+        return;
+    }
+})
+// 닉네임 검사
+
+registerForm['contactSend'].addEventListener('click', e => {
+    dialogCover.show();
+    if (registerForm['contact'].value === '') {
+        duplicateLayer.show({
+            title: '성공',
+            content: '전화번호를 입력해주세요',
+            onConfirm: e => {
+                e.preventDefault();
+                dialogCover.hide();
+                duplicateLayer.hide();
+            }
+        })
+    }
+})
+//전화번호 검사
+
+registerForm.onsubmit = e => {
+    e.preventDefault(); //폼 새로고침 방지
+    if (registerForm['password'].value === '') {
+        dialogCover.show();
+        duplicateLayer.show({
+            title: '성공',
+            content: '비밀번호를 입력해주세요',
+            onConfirm: e => {
+                e.preventDefault();
+                dialogCover.hide();
+                duplicateLayer.hide();
+            }
+        })
+        return;
+    }
+    //비밀번호 체크
+
+    if (registerForm['passwordCheck'].value === '') {
+        dialogCover.show();
+        duplicateLayer.show({
+            title: '성공',
+            content: '비밀번호를 한번 더 입력해주세요',
+            onConfirm: e => {
+                e.preventDefault();
+                dialogCover.hide();
+                duplicateLayer.hide();
+            }
+        })
+        return;
+    }
+    //비밀번호 한번더 체크
+
+    if (registerForm['password'].value !== registerForm['passwordCheck'].value) {
+        dialogCover.show();
+        duplicateLayer.show({
+            title: '성공',
+            content: '비밀번호가 일치하지 않습니다',
+            onConfirm: e => {
+                e.preventDefault();
+                dialogCover.hide();
+                duplicateLayer.hide();
+            }
+        })
+        return;
+    }
+    //비밀번호 일치하는지 체크
+
+    if (registerForm['name'].value === '') {
+        dialogCover.show();
+        duplicateLayer.show({
+            title: '성공',
+            content: '이름을 입력해주세요',
+            onConfirm: e => {
+                e.preventDefault();
+                dialogCover.hide();
+                duplicateLayer.hide();
+            }
+        })
+        return;
+    }
+    //이름 체크 및 유효성
+
+    if(registerForm['birth'].value === ''){
+        dialogCover.show();
+        duplicateLayer.show({
+            title: '성공',
+            content: '생일을 입력해주세요',
+            onConfirm: e => {
+                e.preventDefault();
+                dialogCover.hide();
+                duplicateLayer.hide();
+            }
+        })
+        return;
+    }
+    //생일 체크
+
+    if(registerForm['gender'].value === ''){
+        dialogCover.show();
+        duplicateLayer.show({
+            title: '성공',
+            content: '성별을 선택해주세요',
+            onConfirm: e => {
+                e.preventDefault();
+                dialogCover.hide();
+                duplicateLayer.hide();
+            }
+        })
+        return;
+    }
+    //성별 체크
+
+    if(registerForm['addressSecondary'].value === ''){
+        dialogCover.show();
+        duplicateLayer.show({
+            title: '성공',
+            content: '상세주소를 입력해주세요',
+            onConfirm: e => {
+                e.preventDefault();
+                dialogCover.hide();
+                duplicateLayer.hide();
+            }
+        })
+        return;
+    }
+    //상세주소 미입력시
+}
+
+
 
 
