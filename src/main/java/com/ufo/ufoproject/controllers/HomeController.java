51bfc6ea058f27af0999b80a1048411f794c56e9
ksans <kssk0020@naver.com>
2023-11-17 06:50:04 +0900

    home-footer modify

--- src/main/java/com/ufo/ufoproject/controllers/HomeController.java
+++ src/main/java/com/ufo/ufoproject/controllers/HomeController.java
@@ -17,11 +17,43 @@ public class HomeController {
         return modelAndView;
     }
 
-    @RequestMapping(value = "/mypage",
+    @RequestMapping(value = "/prize",
             method = RequestMethod.GET,
             produces = MediaType.TEXT_HTML_VALUE)
-    public ModelAndView getMyPage() {
-        ModelAndView modelAndView = new ModelAndView("templates/user/mypage");
+    public ModelAndView getPrize() {
+        ModelAndView modelAndView = new ModelAndView("templates/user/prize");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/review",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getReview() {
+        ModelAndView modelAndView = new ModelAndView("templates/user/review");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/bookmark",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getBookmark() {
+        ModelAndView modelAndView = new ModelAndView("templates/user/bookmark");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/following",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getFollowing() {
+        ModelAndView modelAndView = new ModelAndView("templates/user/following");
+        return modelAndView;
+    }
+
+    @RequestMapping(value = "/follower",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getFollower() {
+        ModelAndView modelAndView = new ModelAndView("templates/user/follower");
         return modelAndView;
     }
 }
