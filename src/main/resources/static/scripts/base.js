eea6cc404d2ad4724eb251372fa67a13759acd06
ksans <kssk0020@naver.com>
2023-11-17 04:24:26 +0900

    home-footer modify

--- src/main/resources/static/scripts/base.js
+++ src/main/resources/static/scripts/base.js
@@ -10,6 +10,14 @@ coverElement.hide = () => {
 const imgBtn = document.querySelector('.img_btn');
 const imgModify = document.querySelector('.img_modify');
 
+imgModify.show = () => {
+    imgModify.classList.add('visible');
+}
+
+imgModify.hide = () => {
+    imgModify.classList.remove('visible');
+}
+
 imgBtn.onclick = () => {
     coverElement.show();
     imgModify.show();
